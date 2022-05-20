203fba81f1690281eea925f02163d6f547796eb9
devSrigadgeter <sridhar@thegeeksquad.tech>
2022-05-20 21:24:33 +0530

    implemented navbar

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,76 @@
+// external imports
+import React from "react";
+import {
+  Box,
+  Container,
+  Divider,
+  FormControl,
+  MenuItem,
+  Select,
+} from "@mui/material";
+
+const styles = {
+  box: {
+    display: "flex",
+    justifyContent: "space-between",
+    pt: 2.5,
+    pb: 2.5,
+  },
+  formControl: {
+    "& .MuiInput-root:before": {
+      borderBottom: "none",
+    },
+    "& .MuiInput-root:hover:before": {
+      borderBottom: "none !important",
+    },
+  },
+  select: {
+    width: "max-content",
+    fontSize: "13px",
+    ml: 1,
+    mr: 1,
+    "& .MuiInput-input": {
+      pb: 0,
+    },
+  },
+  divider: {
+    mb: 2,
+  },
+};
+
+const list = [
+  "All Medicines",
+  "Covid Test & Prevention",
+  "Healthcare Products",
+  "Featured",
+  "Fitness & Supplements",
+  "Diabetes",
+  "Personal Care",
+];
+
+// Navbar Component
+const NavBar = () => (
+  <Box>
+    <Container>
+      <Box sx={styles.box}>
+        {list.map((item) => (
+          <FormControl
+            key={item}
+            variant="standard"
+            size="small"
+            sx={styles.formControl}
+          >
+            <Select value={item} sx={styles.select}>
+              <MenuItem key={item} value={item}>
+                {item}
+              </MenuItem>
+            </Select>
+          </FormControl>
+        ))}
+      </Box>
+    </Container>
+    <Divider sx={styles.divider} />
+  </Box>
+);
+
+export default NavBar;
